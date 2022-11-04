506778bb5675357097372ae0bcaafac26a9f9ef3
askarisayed <95429950+askarisayed@users.noreply.github.com>
2022-11-04 13:08:26 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -39,8 +39,8 @@ def main():
     uploaded_file = st.file_uploader("Choose a file", "pdf")
     if uploaded_file is not None:
         for page_layout in extract_pages(uploaded_file):
-        for element in page_layout:
-            st.write(element)
+            for element in page_layout:
+                st.write(element)
 
 
 
