4c9d6c4351b25555df94b7d86c4ce13696b1fad7
askarisayed <95429950+askarisayed@users.noreply.github.com>
2022-11-04 13:07:51 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -2,16 +2,17 @@ import streamlit as st
 import warnings
 from streamlit import components
 import docx2txt
-from PyPDF2 import PdfFileReader
+import pdfminer
+from pdfminer.high_level import extract_pages
 
-def read_pdf(pdf_file):
-    pdfReader = PdfFileReader(pdf_file)
-    count = pdfReader.numPages
-    all_page_text = ""
-    for i in range(count):
-        page = pdfReader.getPage(i)
-        all_page_text += page.extractText()
-    return all_page_text
+# def read_pdf(pdf_file):
+#     pdfReader = PdfFileReader(pdf_file)
+#     count = pdfReader.numPages
+#     all_page_text = ""
+#     for i in range(count):
+#         page = pdfReader.getPage(i)
+#         all_page_text += page.extractText()
+#     return all_page_text
 
 
 def main():
@@ -35,19 +36,11 @@ def main():
             st.write(raw_text)
     
     # Upload the Resumes in PDF or Text Format. 
-    st.subheader("Upload the Resumes")
-    pdf_file = st.file_uploader("Resumes",accept_multiple_files=True)
-    if st.button("Submit"):
-        if pdf_file is not None:
-#             file_details = {"filename":pdf_file.name,"filetype":pdf_file.type,"filesize":pdf_file.size}
-#             st.write(file_details)
-            if pdf_file.type == "text/pdf":
-                raw_text = str(pdf_file.read(),"utf-8")
-                st.text(raw_text)
-        elif pdf_file.type == "application/pdf":
-            pass
-        raw_text = read_pdf(pdf_file)
-        st.write(raw_text)
+    uploaded_file = st.file_uploader("Choose a file", "pdf")
+    if uploaded_file is not None:
+        for page_layout in extract_pages(uploaded_file):
+        for element in page_layout:
+            st.write(element)
 
 
 
