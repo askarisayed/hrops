faa519b7951409fad40acc5421262bd06b2024fb
askarisayed <95429950+askarisayed@users.noreply.github.com>
2022-11-04 14:33:22 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -43,7 +43,7 @@ def main():
             for element in page_layout:
                 st.write(element)
     
-    sim = backend.check_sim(JD_process,resumes)
+    sim = backend.check_sim(docx_file,uploaded_file)
     
     if st.button("Get the Top Resumes"):
         st.write("here are top resumes",sim)
